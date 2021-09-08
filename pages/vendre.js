0435d910d66f66ba0a59f5a65298ccfab08df7b0
Camille Sylla <camillesylla.web@gmail.com>
2021-09-08 14:49:10 +0200

    added header section

--- pages/vendre.js
+++ pages/vendre.js
@@ -1,8 +1,16 @@
+import Header from "../componants/Header/Header";
+import Spacing from "../componants/Spacing/spacing";
+import Form from "../componants/Vendre/Form/Form";
 export default function Vendre () {
 
     return (
         <div>
-            
+            <Spacing value="10vh" />
+            <Header
+        pageTitle="Vendre mon bien avec Demetis Immo"
+        desc="Vous souhaitez vendre votre bien ? Nous somme la pour vous accompagner !"
+      />
+      <Form/>
         </div>
     )
 }
