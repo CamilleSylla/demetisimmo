7d32bbaa819f11aa501be8150b409ace4d0ba21c
Camille Sylla <camillesylla.web@gmail.com>
2021-09-08 14:23:15 +0200

    removed Module

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import Footer from '../componants/Footer/Footer'
-import ModuleVendre from '../componants/ModuleVendre/Vendre/ModuleVendre'
 import Nav from '../componants/Nav/Nav'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    {/* <ModuleVendre/> */}
     <Nav/>
     <Component {...pageProps} />
-
     </>
   )
 }
