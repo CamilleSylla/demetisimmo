f2a0cb6b164732da46b116c2022083cc76d2a626
Camille Sylla <camillesylla.web@gmail.com>
2021-09-15 17:00:37 +0200

    create back document.js

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Footer from "../componants/Footer/Footer";
+
+export default class CustomDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head></Head>
+        <body>
+          <Main />
+        </body>
+        <NextScript />
+      </Html>
+    );
+  }
+}
