3eedcae76fc7f9b4c8b0f1342f9289446985ea0d
Camille Sylla <camillesylla.web@gmail.com>
2022-01-13 02:09:32 +0100

    home page done for responsive

--- componants/Banner/Banner.js
+++ componants/Banner/Banner.js
@@ -1,6 +1,7 @@
 import style from "./banner.module.scss";
 import Search from "../Search/Search";
 import Layout from "../Layout/Layout";
+import { useEffect, useState } from "react";
 
 export default function Banner() {
   const content = {
@@ -8,10 +9,20 @@ export default function Banner() {
     p: "Vous souhaitez vendre ou acheter un bien, nous proposons à nos clients un accompagnement de A à Z.",
   };
 
+  const [logo, setLogo] = useState(null)
+  useEffect(() => {
+    const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches
+    if (isMobile) {
+      setLogo(true)
+    }
+  }, [])
+
   return (
     <Layout>
       <img className={style.img} src="/img/banner5.jpg" />
+      
       <div className={style.title}>
+      {logo ?  <img className={style.logo} src="/Logo/Logo.svg"/> : null}
         <h1>{content.h1}</h1>
         <p>{content.p}</p>
         <button>En savoir plus</button>
