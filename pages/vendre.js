b11773bf5781e6e8063a978d1da5421d79032b8e
Camille Sylla <camillesylla.web@gmail.com>
2022-01-24 02:05:02 +0100

    change a lil bit the UI

--- pages/vendre.js
+++ pages/vendre.js
@@ -5,33 +5,32 @@ import Spacing from "../componants/Spacing/spacing";
 import Form from "../componants/Vendre/Form/Form";
 import Selled from "../componants/Vendre/Selled/Selled";
 import Accompagnement from "../componants/Vendre/Vendu/Vendu";
-export default function Vendre ({houses}) {
-
-    return (
-        <div>
-            <Spacing value="10vh" />
-            <Header
+export default function Vendre({ houses }) {
+  return (
+    <div>
+      <Spacing value="10vh" />
+      <Header
         pageTitle="Vendre mon bien avec Demetis Immo"
         desc="Vous souhaitez vendre votre bien ? Nous sommes la pour vous accompagner !"
-        />
-            <Spacing value="10vh" />
-        <Accompagnement/>
-      <Fonctionnement/>
-      <Spacing value="10vh"/>
-      <Selled houses={houses}/>
-      <Spacing value="10vh"/>
-      <Form/>
-        </div>
-    )
+      />
+      <Spacing value="10vh" />
+      <Accompagnement />
+      <Fonctionnement />
+      <Spacing value="10vh" />
+      <Selled houses={houses} />
+      <Spacing value="10vh" />
+      <Form />
+    </div>
+  );
 }
 
-export async function getServerSideProps () {
-
-    const houses = await axios.get(`${process.env.NEXT_PUBLIC_API}/biens`)
-    .then(res => res.data.filter(el => el.acf.dispo != "Vrai"))
-    return {
-        props: {
-            houses
-        }
-    }
+export async function getServerSideProps() {
+  const houses = await axios
+    .get(`${process.env.NEXT_PUBLIC_API}/biens`)
+    .then((res) => res.data.filter((el) => el.acf.dispo != "Vrai"));
+  return {
+    props: {
+      houses,
+    },
+  };
 }
